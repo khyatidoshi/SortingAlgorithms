763c69b9c855e558d01df69e15f48e9e610b5db6
Khyati Doshi <30694154+khyatidoshi@users.noreply.github.com>
2024-01-06 06:08:17 -0800

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,8 +1,14 @@
 def bubbleSort(arr):
+    # print(" Array to Sort : ", arr)
     for i in range(0,len(arr)):
+        swap = False
         for j in range(0,len(arr)-1-i):
             if arr[j]>arr[j+1]:
+                swap = True
                 tmp=arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1]=tmp
+        # print("Pass",i," array = ", arr)
+        if not swap:
+            break
     return (arr)
